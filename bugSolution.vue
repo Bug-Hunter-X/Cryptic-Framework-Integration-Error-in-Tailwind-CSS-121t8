a78b7e7d8fc9645d4a2d3b8f52ec3325f265721c
BugHunter-X <askzenai@gmail.com>
2025-01-14 09:52:05 +0100

    Add bugSolution.vue file

---
+++ bugSolution.vue
@@ -0,0 +1,21 @@
+```vue
+<template>
+  <div class="text-gray-500 bg-red-500 p-4">
+    This is some text
+  </div>
+</template>
+```
+
+```vue
+<template>
+  <div class="text-gray-500 bg-red-500 p-4">
+    This is some text
+  </div>
+</template>
+
+<script>
+// Correct class name, make sure that `bg-red-500` is defined in your tailwind.config.js
+export default {
+}
+</script>
+```
